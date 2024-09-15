c8a7016b258105deee185af9b96c5290a5737bc3
DevNitrate <ambroiseinthewild@gmail.com>
2024-09-15 16:27:27 +0200

    Improved verion: -added realloc -changed api from MemoryApi to HeapApi -better error handling

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,58 +1,159 @@
 use std::{ffi::c_void, marker::PhantomData, ptr};
-use windows_sys::Win32::{Foundation::BOOL, System::Memory::*};
+use windows_sys::Win32::{Foundation::{BOOL, HANDLE}, System::Memory::*};
 
 pub struct Sallocator<T> {
     size: usize,
     free: *mut c_void,
     addr: *mut T,
+    heap: HANDLE,
     phantom: PhantomData<T>
 }
 
+#[derive(Debug)]
+pub enum HeapError {
+    SallocCreateFailed,
+    SallocMallocFailed,
+    SallocReallocFailed,
+    SallocSetUnallocated,
+    SallocGetUnallocated,
+    SallocFreeUnallocated,
+    SallocSetOutOfBound,
+    SallocGetOutOfBound,
+    SallocFreeFailed,
+    SallocDestroyFailed
+}
+
+impl std::fmt::Display for HeapError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match *self {
+            Self::SallocCreateFailed => write!(f, "SallocCreateFailed"),
+            Self::SallocMallocFailed => write!(f, "SallocMallocFailed"),
+            Self::SallocReallocFailed => write!(f, "SallocReallocFailed"),
+            Self::SallocSetUnallocated => write!(f, "SallocSetUnallocated"),
+            Self::SallocGetUnallocated => write!(f, "SallocGetUnallocated"),
+            Self::SallocFreeUnallocated => write!(f, "SallocFreeUnallocated"),
+            Self::SallocSetOutOfBound => write!(f, "SallocSetOutOfBound"),
+            Self::SallocGetOutOfBound => write!(f, "SallocGetOutOfBound"),
+            Self::SallocFreeFailed => write!(f, "SallocFreeFailed"),
+            Self::SallocDestroyFailed => write!(f, "SallocDestroyFailed"),
+        }
+    }
+}
+
 impl<T: std::fmt::Debug + Clone + Copy> Sallocator<T> {
-    pub fn new(size: usize) -> Self {
+    pub fn new() -> Result<Self, HeapError> {
         let addr: *mut T = ptr::null_mut();
         let free: *mut c_void = ptr::null_mut();
+        let heap: HANDLE;
+        unsafe {
+            heap = HeapCreate(HEAP_CREATE_ENABLE_EXECUTE, 0, 0);
+        }
 
-        Sallocator {
-            size,
+        if heap.is_null() {
+            return Err(HeapError::SallocCreateFailed);
+        }
+
+        return Ok(Sallocator {
+            size: 0,
             free,
             addr,
+            heap,
             phantom: PhantomData
-        }
+        });
     }
 
-    pub fn salloc(&mut self) {
+    pub fn malloc(&mut self, size: usize) -> Result<(), HeapError> {
+        self.size = size;
+
         unsafe {
-            self.free = VirtualAlloc(self.free, self.size, MEM_COMMIT, PAGE_READWRITE);
+            self.free = HeapAlloc(self.heap, HEAP_ZERO_MEMORY, self.size);
             self.addr = self.free.cast();
         }
+
+        if self.free.is_null() {
+            return Err(HeapError::SallocMallocFailed);
+        } else {
+            return Ok(());
+        }
     }
 
-    pub fn set(&mut self, index: isize, el: T) {
+    pub fn realloc(&mut self, size: usize) -> Result<(), HeapError> {
+        self.size = size;
+
+        unsafe {
+            self.free = HeapReAlloc(self.heap, HEAP_ZERO_MEMORY, self.free, self.size);
+            self.addr = self.free.cast();
+        }
+
+        if self.free.is_null() {
+            return Err(HeapError::SallocReallocFailed);
+        } else {
+            return Ok(());
+        }
+    }
+
+    pub fn set(&mut self, index: isize, el: T) -> Result<(), HeapError> {
+        if self.addr.is_null() {
+            return Err(HeapError::SallocSetUnallocated);
+        }
+
+        if index as usize >= self.size / size_of::<T>() as usize || index < 0 {
+            return Err(HeapError::SallocSetOutOfBound);
+        }
+
         unsafe {
             *self.addr.offset(index) = el;
         }
+
+        return Ok(());
     }
 
-    pub fn get(&mut self, index: isize) -> T {
+    pub fn get(&mut self, index: isize) -> Result<T, HeapError> {
+        if self.addr.is_null() {
+            return Err(HeapError::SallocGetUnallocated);
+        }
+
+        if index as usize >= self.size / size_of::<T>() as usize || index < 0 {
+            return Err(HeapError::SallocGetOutOfBound);
+        }
+
         unsafe {
-            *self.addr.offset(index)
+            return Ok(*self.addr.offset(index));
         }
     }
 
-    pub fn free(&mut self) {
+    pub fn free(&mut self) -> Result<(), HeapError> {
         if self.free.is_null() {
-            panic!("cannot free memory that has already been freed");
+            return Err(HeapError::SallocFreeUnallocated);
         }
 
         unsafe {
-            let free: BOOL = VirtualFree(self.free, self.size, MEM_RELEASE);
+            let free: BOOL = HeapFree(self.heap, 0, self.free);
+    
+            self.free = ptr::null_mut();
+            self.addr = ptr::null_mut();
 
-            if free.is_negative() {
-                panic!("failed to free memory");
+            if free == 0 {
+                return Err(HeapError::SallocFreeFailed);
+            } else {
+                return Ok(());
             }
         }
+    }
 
-        self.free = ptr::null_mut();
+    pub fn destroy(&mut self) -> Result<(), HeapError> {
+        unsafe {
+            let free: BOOL = HeapDestroy(self.heap);
+    
+            self.free = ptr::null_mut();
+            self.addr = ptr::null_mut();
+            self.size = 0;
+
+            if free == 0 {
+                return Err(HeapError::SallocDestroyFailed);
+            } else {
+                return Ok(());
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,16 @@ use allocator::Sallocator;
 mod allocator;
 
 fn main() {
-    let mut sal: Sallocator<u32> = Sallocator::new(12);
-    sal.salloc();
-    sal.set(1, 123);
-    println!("{}", sal.get(1));
-    sal.free();
+    let mut sal: Sallocator<u32> = Sallocator::new().unwrap();
+    sal.malloc(size_of::<u32>() * 10).unwrap();
+    sal.set(9, 123).unwrap();
+
+    sal.realloc(size_of::<u32>() * 11).unwrap();
+    sal.set(11, 124).unwrap();
+
+    println!("9: {}", sal.get(9).unwrap());
+    println!("10: {}", sal.get(10).unwrap());
+
+    sal.free().unwrap();
+    sal.destroy().unwrap();
 }
